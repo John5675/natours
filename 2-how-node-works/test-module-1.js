0493e8787bf8dd1d6f8cd98b6f1e1581110979b9
jhuynh <79059047+John5675@users.noreply.github.com>
2024-07-04 17:21:10 -0700

    Completed 2-how-node-works

---
+++ 2-how-node-works/test-module-1.js
@@ -0,0 +1,27 @@
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multiply(a, b) {
+//     a * b;
+//   }
+
+//   divide(a, b) {
+//     a / b;
+//   }
+// }
+
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    a * b;
+  }
+
+  divide(a, b) {
+    a / b;
+  }
+};
