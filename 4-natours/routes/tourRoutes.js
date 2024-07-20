0d9ac0cf54053fc8f320543e9be0f45e4625ff68
jhuynh <79059047+John5675@users.noreply.github.com>
2024-07-20 00:00:34 -0700

    Database Indexing

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -7,10 +7,6 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-// POST /tour/20398475/reviews
-// GET /tour/20398475/reviews
-// GET /tour/20398475/reviews/10923874
-
 router.use('/:tourId/reviews', reviewRouter);
 
 router
@@ -18,17 +14,31 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
 router
   .route('/')
-  .get(authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
